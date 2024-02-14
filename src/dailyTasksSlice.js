3d6beb6d0702cf222f45c2c91b9dcba02b147ebf
manish <manish.ramani.7090@gmail.com>
2024-02-14 09:37:31 +0530

    browser persistent storage of tasks via localStorage

--- src/dailyTasksSlice.js
+++ src/dailyTasksSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  tasks: ["meditate", "hck", "dinner"],
+  tasks: JSON.parse(localStorage.getItem('tasks_007')) || ["meditate", "hck", "dinner"],
 };
 
 const dailyTaskSlice = createSlice({
@@ -10,10 +10,12 @@ const dailyTaskSlice = createSlice({
   reducers: {
     addTask(state, action) {
       state.tasks = [...state.tasks, action.payload]
+      localStorage.setItem('tasks_007', JSON.stringify(state.tasks));
     },
     closeTask(state, action) {
       const newDailyTasks = state.tasks.filter(item => item!==action.payload);
       state.tasks = newDailyTasks
+      localStorage.setItem('tasks_007', JSON.stringify(state.tasks));
     }
   }
 });
